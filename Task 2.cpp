62042b9760376b13f72d50783c7cdc3e95b391c6
Dhanusase <150422445+Dhanusase@users.noreply.github.com>
2023-11-10 13:08:30 +0530

    Add files via upload

---
+++ Task 2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	string operation;
+	double number1,number2;
+	
+top:cout << "\nEnter First Number :" << endl;
+	cin >> number1;
+	cout << "Enter Second Number :" << endl;
+	cin >> number2;
+	cout << "Choose the operation (+ , - , * , /)" << endl;
+	cin  >> operation;
+	cout << "Answer for above operation is :"<< endl;
+	
+	if(operation == "+")
+	{
+		cout << number1 + number2;
+	}
+	else if(operation == "-")
+	{
+		cout << number1 - number2;
+	}
+	else if(operation == "*")
+	{
+		cout << number1 * number2;
+	}
+	else if(operation == "/")
+	{
+		cout << number1 / number2;
+	}
+	else
+	{
+		cout << "Invalid operation" << endl;
+	}
+	
+	goto top;
+	
+	return 0;
+}
